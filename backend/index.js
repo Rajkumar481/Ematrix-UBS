82e7deb5305190cab1f89ae1837991e94b4d97d0
raghulematix <raghulkumarematix@gmail.com>
2025-07-01 13:37:23 +0530

    raghul

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+dotenv.config();
 import cors from 'cors';
-import connectDB from './db.js';
+// import connectDB from './db.js';
 
 import userRoutes from './Routes/UserRoutes.js';
 import purchaseRoutes from './Routes/PurchaseRoutes.js';
 import companyRoutes from './Routes/CompanyRoutes.js';
+import mongoose from 'mongoose';
 
-dotenv.config();
 
 const app = express();
-connectDB();
+// connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -24,6 +25,17 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+try {
+  await mongoose.connect(process.env.MONGO_URI)
+  
+  app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+} catch (error) { 
+  console.log(error);
+  process.exit(1);//close server
+  
+}
